61812b2879272bc01a2fff5e647ab1200165587d
heeshala <heeshala25@gmail.com>
2019-10-23 16:19:28 +0530

    c code

--- array.c
+++ array.c
@@ -2,5 +2,27 @@
 
 void  main()
 {
-    
+    int arr[10];
+    int i;
+    while(i=0;i<10;i++)
+    printf("Enter a number to array\n");
+    scanf("%d",&arr[i])
+
+    while(i=0;i<10;i++)
+    printf("%d",&arr[i]);
+
+}
+
+
+void  main()
+{
+    int arr[n];
+    int i,n;
+    while(i=0;i<n;i++)
+    printf("Enter a number to array\n");
+    scanf("%d",&arr[i])
+
+    while(i=n-1;i>0;i--)
+    printf("%d",&arr[i]);
+
 }
